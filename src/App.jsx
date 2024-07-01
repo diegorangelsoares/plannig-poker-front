7b036ef02c1db9c8268c6f369860326af74ec742
diegofreire <diego.freire@rpe.tech>
2024-07-01 16:31:22 -0300

    Criando component do topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import {Topbar} from "./components/topbar";
+
 function App() {
   return (
-    <div className="App">
-      Ola React
-
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-    </div>
+    <>
+        <Topbar/>
+    </>
   );
 }
 
---
+++ src/components/topbar/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export function Topbar(){
+    return (
+        <div>
+            <a href="www.google.com">www.google.com</a>
+        </div>
+    );
+}
