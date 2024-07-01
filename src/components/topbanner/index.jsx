6b53d8ba2685f7c0357ced0e85ae135f876a616a
diegofreire <diego.freire@rpe.tech>
2024-07-01 17:25:11 -0300

    Ajuste no banner para adicionar imagem com responsividade

--- src/components/topbanner/index.jsx
+++ src/components/topbanner/index.jsx
@@ -1,8 +1,10 @@
+import logoplanningpoker from "./images/Planning_Poker_logo.png"
 import "./style.css";
 
 export function Topbanner(){
     return (
         <div className="topbanner">
+            <img src={logoplanningpoker} alt="Planning poker" className="topbanner" />
             <center className="topbanner" >PLANNING POKER X</center>
         </div>
     );
--- src/components/topbar/index.jsx
+++ src/components/topbar/index.jsx
@@ -1,9 +1,12 @@
 import "./style.css";
 
-export function Topbar(){
+export function Topbar() {
     return (
         <div className="topbar">
-            <a href="http://www.google.com" className="topbar">www.google.com</a>
+            <a href="" className="topbar"></a>
+
         </div>
     );
 }
+//Para adicionar link de site no top bar no inicio da div
+// <a href="http://www.google.com" className="topbar">www.google.com</a>
