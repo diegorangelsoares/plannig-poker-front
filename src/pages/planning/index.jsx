acea7e306722fc14edf0560b15d71b690471aedb
diegofreire <diego.freire@rpe.tech>
2024-07-02 15:19:22 -0300

    Adicionando useStatus para pegar status de variaveis

--- src/pages/planning/index.jsx
+++ src/pages/planning/index.jsx
@@ -1,12 +1,32 @@
 import "./style.css";
 import {BACKEND_URL} from "../../config";
 import axios from "axios";
+import {useState} from "react";
+
+function planningHandler(plannings){
+     if (!Array.isArray(plannings)){
+         return plannings;
+     }
+
+     var i = 0;
+
+     var content = plannings.map(function (planning){
+         return (
+             <div key={i++} > {planning.titulo}</div>
+         );
+     });
+
+    return content;
+}
 
 export function Planning(){
 
+    const [plannings, setPlannings] = useState([]);
+
     axios.get(BACKEND_URL + "/api/v1/planning/102")
     .then(function (response){
-        console.log(response);
+        console.log("RODANDO");
+        setPlannings(planningHandler(response.data));
     })
     .catch(function (error){
         console.log(error);
