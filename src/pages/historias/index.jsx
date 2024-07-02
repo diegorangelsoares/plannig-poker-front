7a49fde564f404d367568685df5afe6d6eacf318
diegofreire <diego.freire@rpe.tech>
2024-07-02 17:13:37 -0300

    Imprimindo dados do backend nas paginas de planning e historias

--- src/pages/historias/index.jsx
+++ src/pages/historias/index.jsx
@@ -1,10 +1,47 @@
 import "./style.css";
+import {BACKEND_URL} from "../../config";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+function historiasHandler(historias){
+    if (!Array.isArray(historias)){
+        return historias;
+    }
+
+    var i = 0;
+
+    var content = historias.map(function (historia){
+        console.log(historia.descricao);
+        return (
+            <div className="historias-content" key={i++}> {i + 1} - {historia.descricao} - {historia.card} - {historia.status}</div>
+        );
+    });
+
+    return content;
+}
 
 export function Historias(){
+
+    const [historias, setHistorias] = useState("Nada a mostrar");
+
+    useEffect(function (){
+        axios.get(BACKEND_URL + "/api/v1/historia/all?page=0&size=20&sort=id")
+            .then(function (response){
+                console.log(response.data.content)
+                setHistorias(historiasHandler(response.data.content));
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+            .finally(function (){
+                console.log("finally")
+            });
+    },[]);
+
     return(
         <div className="content">
             <h1>HISTÓRIAS</h1>
-            <div> CONTEUDO </div>
+            {historias}
         </div>
     );
 }
--- src/pages/votacao/index.jsx
+++ src/pages/votacao/index.jsx
@@ -1,10 +1,47 @@
 import "./style.css";
+import {BACKEND_URL} from "../../config";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+function votosHandler(votos){
+    if (!Array.isArray(votos)){
+        return votos;
+    }
+
+    var i = 0;
+
+    var content = votos.map(function (voto){
+        console.log(voto.votante);
+        return (
+            <div className="votos-content" key={i++}> {i + 1} - {voto.votante} - {voto.orcamento}</div>
+        );
+    });
+
+    return content;
+}
 
 export function Votacao(){
+
+    const [votos, setVotos] = useState("Nada a mostrar");
+
+    useEffect(function (){
+        axios.get(BACKEND_URL + "/api/v1/votacao/historia/1/final")
+            .then(function (response){
+                console.log(response.data.content)
+                setVotos(votosHandler(response.data.content));
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+            .finally(function (){
+                console.log("finally")
+            });
+    },[]);
+
     return(
         <div className="content">
             <h1>VOTAÇÃO</h1>
-            <div> CONTEUDO </div>
+            {votos}
         </div>
     );
 }
