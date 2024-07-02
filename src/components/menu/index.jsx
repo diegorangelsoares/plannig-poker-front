6ecba538bd00915d9cb472fedc1370dd844b9e29
diegofreire <diego.freire@rpe.tech>
2024-07-02 11:24:01 -0300

    Ajuste de design dos menu

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
 import "./style.css";
 
 export function Menu(){
     return (
-        <div>HOME</div>
+        <div className="menu">
+            <nav className="menu">
+                <Link to="/" className="menu">HOME</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">PLANNING</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">HISTÓRIAS</Link>
+            </nav>
+            <nav className="menu">
+                <Link to="/" className="menu">VOTAÇÃO</Link>
+            </nav>
+
+        </div>
     );
 }
