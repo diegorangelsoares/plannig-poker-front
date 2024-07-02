268f31a3735f289624e9cc126bd1c933850f866f
diegofreire <diego.freire@rpe.tech>
2024-07-02 15:07:52 -0300

    Configurando o axios para consumo do backend

--- src/pages/planning/index.jsx
+++ src/pages/planning/index.jsx
@@ -1,12 +1,25 @@
 import "./style.css";
 import {BACKEND_URL} from "../../config";
+import axios from "axios";
 
 export function Planning(){
-    console.log("Endereco do backend: "+ BACKEND_URL);
+
+    axios.get(BACKEND_URL + "/api/v1/planning/102")
+    .then(function (response){
+        console.log(response);
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+    .finally(function (){
+        console.log("finally")
+    });
+
     return(
         <div className="content">
             <h1>PLANNING</h1>
             <div> CONTEUDO </div>
         </div>
     );
+
 }
