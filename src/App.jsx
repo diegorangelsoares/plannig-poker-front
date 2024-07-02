cab4a18c910647841c81f186f8253b099445aac0
diegofreire <diego.freire@rpe.tech>
2024-07-02 10:29:16 -0300

    Criação inicial do body onde vão conter os menus

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {Topbar} from "./components/topbar";
 import "./global.css"
 import {Topbanner} from "./components/topbanner";
+import {Mainbody} from "./components/mainbody";
 
 function App() {
   return (
     <>
         <Topbar/>
         <Topbanner/>
+        <Mainbody/>
     </>
   );
 }
