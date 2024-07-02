45534ddccd5704baab5aaaa4a1cca772be92b2a4
diegofreire <diego.freire@rpe.tech>
2024-07-02 14:51:45 -0300

    Configurando variaveld e ambiente

--- src/pages/planning/index.jsx
+++ src/pages/planning/index.jsx
@@ -1,6 +1,8 @@
 import "./style.css";
+import {BACKEND_URL} from "../../config";
 
 export function Planning(){
+    console.log("Endereco do backend: "+ BACKEND_URL);
     return(
         <div className="content">
             <h1>PLANNING</h1>
