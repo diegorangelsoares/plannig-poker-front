061156f4a4e77ee07702db95746031c765864ef4
diegofreire <diego.freire@rpe.tech>
2024-07-04 10:10:23 -0300

    Construindo o formulario de cadastro de planning

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,12 @@ export function Home(){
     return(
         <div className="content">
             <h1>HOME</h1>
-            <div> CONTEUDO </div>
+            <div className="home-form">
+                <h3 className="home-form">Inserir nova Planning: </h3>
+                <p className="home-form"><input className="home-form" size={40} type="text" placeholder="Titulo"></input></p>
+                <p className="home-form"><input className="home-form" size={50} type="text" placeholder="Observação"></input></p>
+                <p className="home-form"><input className="home-form" size={20} type="text" placeholder="Equipe"></input></p>
+            </div>
         </div>
     );
 }
