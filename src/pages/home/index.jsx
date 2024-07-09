064d1689cc202e3342b15378d1a489df2788feb9
diegofreire <diego.freire@rpe.tech>
2024-07-09 15:26:28 -0300

    Coletando os dados dos campos do formulario

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -16,8 +16,8 @@ function createHistoriaInputs(historiasSize, setHistoriasSize) {
     for (var i = 0; i < historiasSize; i++) {
         content.push(
             <p className="home-form" key={i}>
-                <input type="text" className="home-form" size={40} placeholder="Descricao"/>
-                <input type="text" className="home-form" size={15} placeholder="Card"/>
+                <input type="text" className="home-form" size={40} name="descricao" placeholder="Descricao"/>
+                <input type="text" className="home-form" size={15} name="card" placeholder="Card"/>
                 <PlusIcon size={25} className="home-form-icon" onClick={()=>setHistoriasSize(historiasSize+1)}/>
                 <MinusIcon size={25} className="home-form-icon" onClick={()=>decreaseHistoriaSize(historiasSize, setHistoriasSize)}/>
             </p>
@@ -25,6 +25,22 @@ function createHistoriaInputs(historiasSize, setHistoriasSize) {
     }
 
     return content;
+}
+
+function formHandle(e){
+    e. preventDefault();
+    // console.log(e.target.equipe.value);
+    //Coletando os dados da planning
+    const titulo = e.target.titulo.value;
+    const observacao = e.target.observacao.value;
+    const equipe = e.target.equipe.value;
+
+    const historias = Array.from(e.target.descricao).map((descricao) => descricao.value);
+    console.log(titulo);
+    console.log(observacao);
+    console.log(equipe);
+    console.log(historias);
+
 }
 
 export function Home() {
@@ -35,18 +51,20 @@ export function Home() {
         <div className="content">
             <h1>HOME</h1>
             <div className="home-form">
-                <h3 className="home-form">Cadastrar Planning: </h3>
-                <p className="home-form"><input className="home-form" size={40} type="text"
-                                                placeholder="Titulo"></input></p>
-                <p className="home-form"><input className="home-form" size={50} type="text"
-                                                placeholder="Observação"></input></p>
-                <p className="home-form"><input className="home-form" size={20} type="text"
-                                                placeholder="Equipe"></input></p>
-                <h3 className="home-form">Histórias: </h3>
+                <form onSubmit={function (e){formHandle(e)}}>
+                    <h3 className="home-form">Cadastrar Planning: </h3>
+                    <p className="home-form"><input className="home-form" size={40} name="titulo" type="text"
+                                                    placeholder="Titulo"></input></p>
+                    <p className="home-form"><input className="home-form" size={50} name="observacao" type="text"
+                                                    placeholder="Observação"></input></p>
+                    <p className="home-form"><input className="home-form" size={20} name="equipe" type="text"
+                                                    placeholder="Equipe"></input></p>
+                    <h3 className="home-form">Histórias: </h3>
 
-                {createHistoriaInputs(historiasSize, setHistoriasSize)}
+                    {createHistoriaInputs(historiasSize, setHistoriasSize)}
 
-                <p className="home-form"><input type="submit" className="home-form-button" value="Cadastrar nova história" /></p>
+                    <p className="home-form"><input type="submit" className="home-form-button" value="Cadastrar nova história" /></p>
+                </form>
             </div>
         </div>
     );
